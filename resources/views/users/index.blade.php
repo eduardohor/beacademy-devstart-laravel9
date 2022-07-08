7a159266a6e03ee03c1996a02351e331a64a4331
eduardohor <eduardo.hor@outlook.com>
2022-07-08 16:16:18 -0300

    searching user

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,7 +3,21 @@
 @section('body')
 
 <h1>Listagem de Usuários</h1>
-  <a href="{{route('users.create')}}" class="btn btn-outline-dark">Novo Usuário</a>
+  <div class="container">
+    <div class="row">
+        <div class="col-sm mt-2 mb-5">
+            <a href="{{route('users.create')}}" class="btn btn-outline-dark">Novo Usuário</a>
+        </div>
+        <div class="col-sm mt-2 mb-5">
+            <form action="{{route('users.index')}}" method="GET">
+                <div class="input-group">
+                    <input type="search" class="form-control rounded" name="search">
+                    <button type="submit" class="btn btn-outline-primary">Pesquisar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+  </div>
 
   <table class="table">
       <thead class="text-center">
