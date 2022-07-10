2a657351fb741b5fc09257e4ddc69c7ce6e33a07
eduardohor <eduardo.hor@outlook.com>
2022-07-10 11:57:13 -0300

    adding authentication

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,41 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\TeamController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ViaCepController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\{
-    PostController,
-    TeamController,
-    UserController,
-    ViaCepController
-};
+
+require __DIR__ . '/auth.php';
+
+
+Route::get('/', function () {
+    return view('home');
+});
 
 //Team
 Route::get('/teams/users/{id}', [TeamController::class, 'showUserToTeams'])->name('teams.user.show');
 Route::get('/teams/{id}', [TeamController::class, 'showTeams'])->name('teams.show');
 
-//POSTS
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('/posts/{id}/posts', [PostController::class, 'show'])->name('posts.show');
 
-//USUÁRIO
-Route::delete('users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-Route::put('users/{id}', [UserController::class, 'update'])->name('users.update');
-Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
-Route::post('/user', [UserController::class, 'store'])->name('user.store');
+
+
+Route::middleware(['auth'])->group(function () {
+
+    //POSTS
+    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+    Route::get('/posts/{id}/posts', [PostController::class, 'show'])->name('posts.show');
+
+    //USUÁRIO
+    Route::delete('users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::put('users/{id}', [UserController::class, 'update'])->name('users.update');
+    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+    Route::post('/user', [UserController::class, 'store'])->name('user.store');
+});
+
 
 //VIA CEP WEB SERVICE
 Route::get('/viacep', [ViaCepController::class, 'index'])->name('viacep.index');
